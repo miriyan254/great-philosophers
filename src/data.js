3e6f4cf207203f0fce005994821899e2bc4f2629
Miriyan Asenov <miriyan254@gmail.com>
2025-04-07 11:40:43 +0300

    1. Initial commit.

---
+++ src/data.js
@@ -0,0 +1,97 @@
+import platoImg from "./assets/plato.jpg";
+import aristotleImg from "./assets/aristotle.jpg";
+import descartesImg from "./assets/descartes.jpg";
+import lockeImg from "./assets/locke.jpg";
+import kantImg from "./assets/kant.jpg";
+import wittgensteinImg from "./assets/wittgenstein.jpg";
+
+export const philosophers = [
+
+	{
+		image: platoImg,
+		width:"300",
+		height:"400",
+		name: "Plato",
+		born: "427 BC in Athens",
+		died: "348 BC in Athens",
+		majorField: "Metaphysics",
+		fundamentalWork: "The Republic",
+		famousQuote: "If we are ever to have true knowledge of anything, we must get rid of the body and contemplate things by themselves with the soul by itself.",
+	},
+	
+	{
+		image: aristotleImg,
+		width:"298",
+		height:"400",
+		name: "Aristotle",
+		born: "384 BC in Stagira",
+		died: "322 in Chalcis",
+		majorField: "Metaphysics",
+		fundamentalWork: "Metaphysics",
+		famousQuote: "All men by nature desire to know.",
+	},
+	
+	{
+		image: descartesImg,
+		width:"203",
+		height:"248",
+		name: "Descartes",
+		born: "1596 in La Haye en Touraine",
+		died: "1650 in Stockholm",
+		majorField: "Epistemology",
+		fundamentalWork: "Meditations on First Philosophy",
+		famousQuote: "I think, therefore I am.",
+	},
+	
+	{
+		image: lockeImg,
+		width:"290",
+		height:"400",
+		name: "Locke",
+		born: "1632 in Wrington",
+		died: "1704 in High Laver",
+		majorField: "Epistemology",
+		fundamentalWork: "An Essay Concerning Human Understanding",
+		famousQuote: "Let us then suppose the mind to be, as we say, white paper, void of all characters, without any ideas...Whence has it all the MATERIALS of reason and knowledge? To this I answer, in one word, from EXPERIENCE.",
+	},
+	
+	{
+		image: kantImg,
+		width:"900",
+		height:"675",
+		name: "Kant",
+		born: "1724 in Koenigsberg",
+		died: "1804 in Koenigsberg",
+		majorField: "Metaphysics",
+		fundamentalWork: "Critique of Pure Reason",
+		famousQuote: "Thoughts without content are empty, intuitions without concepts are blind.",
+	},
+	
+	{
+		image: wittgensteinImg,
+		width:"200",
+		height:"358",
+		name: "Wittgenstein",
+		born: "1889 in Vienna",
+		died: "1951 in Cambridge",
+		majorField: "Analytic philosophy",
+		fundamentalWork: "Tractatus Logico-Philosophicus",
+		famousQuote: "The limits of my language mean the limits of my world.",
+	}
+];	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
