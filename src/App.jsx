60da45547db8323534e30a27c60c734514020424
Miriyan Asenov <miriyan254@gmail.com>
2025-04-09 00:04:05 +0300

    7. modified: src/components/PhilosophersList.jsx, src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import {philosophers} from './data.js';
 import Header from './components/Header';
 import PhilosophersList from './components/PhilosophersList';
+import PhilosopherDetails from './components/PhilosopherDetails';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <PhilosophersList />
+        <PhilosophersList philosophers={philosophers}/>
+        <PhilosopherDetails selectedPhilosopher={philosophers[5]} />
       </main>  
     </>
   );
--- src/components/PhilosophersList.jsx
+++ src/components/PhilosophersList.jsx
@@ -1,7 +1,6 @@
-import {philosophers} from '../data.js';
 import Philosopher from './Philosopher';
 
-function PhilosophersList(){
+function PhilosophersList({philosophers}){
 	return <ul>{ philosophers.map( phil => <li key={phil.name}><Philosopher philosopher={phil} /></li> ) }</ul>;
 }
 
