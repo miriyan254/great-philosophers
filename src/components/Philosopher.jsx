b4be54ab8f1596d2948b95f9243a596bf5503ae8
miriyan254 <miriyan254@gmail.com>
2025-04-09 01:27:58 +0300

    Update Philosopher.jsx

--- src/components/Philosopher.jsx
+++ src/components/Philosopher.jsx
@@ -1,9 +1,9 @@
-function Philosopher({philosopher}) {
+function Philosopher({philosopher, handleOnClick}) {
   return (
 		<>
 			<img src={philosopher.image} alt={philosopher.name} width={philosopher.width} height={philosopher.height} />
 			<h3>{philosopher.name}</h3>
-			<button type='button'>More</button>
+			<button type='button' onClick={handleOnClick}>More</button>
 		</>
 	);
 }
